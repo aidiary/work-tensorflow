f44586ba9ed9fdc0670ba70238b02e4fca0a7b61
Koichiro Mori <f2forest@gmail.com>
2020-02-18 16:48:50 +0900

    Add imdb.py

---
+++ imdb.py
@@ -0,0 +1,57 @@
+from comet_ml import Experiment
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Embedding, Bidirectional, LSTM, Dense
+from tensorflow.keras.optimizers import Adam
+
+
+BUFFER_SIZE = 10000
+BATCH_SIZE = 64
+NUM_EPOCHS = 10
+
+
+experiment = Experiment(project_name='tf2-imdb',
+                        workspace='koichiro-mori',
+                        auto_param_logging=False)
+
+
+def load_dataset():
+    dataset, info = tfds.load('imdb_reviews/subwords8k', with_info=True, as_supervised=True)
+    train_ds, test_ds = dataset['train'], dataset['test']
+
+    # padded_batch()はミニバッチで最長の系列に合わせてpaddingする
+    train_ds = train_ds.shuffle(BUFFER_SIZE).padded_batch(BATCH_SIZE, padded_shapes=([None], []))
+    test_ds = test_ds.padded_batch(BATCH_SIZE, padded_shapes=([None], []))
+
+    encoder = info.features['text'].encoder
+
+    return train_ds, test_ds, encoder.vocab_size
+
+
+def main():
+    train_ds, test_ds, vocab_size = load_dataset()
+
+    model = Sequential([
+        Embedding(vocab_size, 64),
+        Bidirectional(LSTM(64)),
+        Dense(64, activation='relu'),
+        Dense(1, activation='sigmoid')
+    ])
+
+    model.compile(optimizer=Adam(1e-4),
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+    model.summary()
+
+    with experiment.train():
+        model.fit(train_ds, epochs=NUM_EPOCHS, validation_data=test_ds)
+
+    with experiment.test():
+        test_loss, test_acc = model.evaluate(test_ds)
+        print('Test Loss: {}'.format(test_loss))
+        print('Test Accuracy: {}'.format(test_acc))
+
+
+if __name__ == "__main__":
+    main()
